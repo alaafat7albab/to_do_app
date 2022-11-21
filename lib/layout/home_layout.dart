5fdd6c33aad478a2e270127565e7f5dabf69d5ee
alaafat7albab <alaa.fathalbab@gmail.com>
2022-11-21 14:31:40 +0200

    toggle & bottomNav

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/modules/archived_tasks/archived_tasks_screen.dart';
+import 'package:to_do_app/modules/done_tasks/done_tasks_screen.dart';
+import 'package:to_do_app/modules/new_tasks/new_tasks_screen.dart';
+
+class HomeLayout extends StatefulWidget {
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex = 0;
+
+  List<Widget> screens =
+  [
+    NewTasksScreen(),
+    DoneTasksScreen(),
+    ArchivedTasksScreen()
+  ];
+
+  List<String> titles =
+  [
+    'New Tasks',
+    'Done Tasks',
+    'Archived Tasks'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          titles[currentIndex],
+        ),
+      ),
+      body: screens[currentIndex],
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          elevation: 15.0,
+          currentIndex: currentIndex,
+          onTap: (index){
+            setState(() {
+               currentIndex = index;
+            });
+          },
+          items:
+          [
+            BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Tasks',
+          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle_outline),
+              label: 'Done',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.archive_outlined),
+              label: 'Archived',
+            ),
+          ],
+      ),
+    );
+  }
+}
